10359a1b71e421bd29f0087f0861449ef91c4f3b
Yassine BIZNAOUNE <yassine.biznaoune@e-polytechnique.com>
2024-02-15 23:19:22 +0100

    first commit

---
+++ frontend/src/app/myusers/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyUsers = () => {
+  return (
+    <main id="MyUsers">
+      <h1>MyUsers</h1>
+    </main>
+  );
+};
+
+export default MyUsers;
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import "./styles/Home.scss";
+
+export default function Home() {
+  return (
+    <main className="Home">
+      <h1 className="main_title">Bienvenido a la página de inicio</h1>
+    </main>
+  );
+}
