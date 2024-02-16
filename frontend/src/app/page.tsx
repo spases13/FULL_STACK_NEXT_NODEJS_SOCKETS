b76e6f952a72387a5d656768c48b5a84d07ddb25
Yassine BIZNAOUNE <yassine.biznaoune@e-polytechnique.com>
2024-02-16 17:40:47 +0100

    add test btn

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,21 @@ export default function Home() {
   return (
     <main className="Home">
       <h1 className="main_title">Bienvenido a la página de inicio</h1>
+      <center>
+        <a href="/myusers">
+          <button
+            style={{
+              backgroundColor: "#000",
+              color: "#fff",
+              fontSize: "1.1rem",
+              padding: 16,
+              margin: "50px auto",
+            }}
+          >
+            TEST CHAT
+          </button>
+        </a>
+      </center>
     </main>
   );
 }
